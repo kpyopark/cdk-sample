11a5d80af2d3452f2779576b22eb2ad7f3456654
kpyopark <toheavener01@gmail.com>
2020-02-25 17:51:20 +0900

    added alb/albtarget/and nat gateway.

--- lib/cdk-sample-ts-stack.ts
+++ lib/cdk-sample-ts-stack.ts
@@ -1,13 +1,20 @@
 import * as cdk from '@aws-cdk/core';
 import ec2 = require('@aws-cdk/aws-ec2');
+import elbv2 = require('@aws-cdk/aws-elasticloadbalancingv2');
 import { truncate } from 'fs';
+
+
+const vpcenv = ( process.env.vpcenv === undefined ) ? 'test' : process.env.vpcenv;
+const corp = (process.env.corpname === undefined ) ? 'samcorp' : process.env.corpname;
+const servicename = (process.env.servicename === undefined ) ? 'albtest' : process.env.servicename;
+const elemPrefix = `${vpcenv}-${corp}-${servicename}-`;
 
 export class CdkSampleTsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    const vpc = new ec2.Vpc(this, 'test-curly-alb-vpc', 
+    const vpc = new ec2.Vpc(this, `${elemPrefix}-vpc`, 
     {
       cidr : '10.0.0.0/16',
       enableDnsHostnames: true,
@@ -57,6 +64,12 @@ export class CdkSampleTsStack extends cdk.Stack {
     publicSubnetA.addDefaultInternetRoute(igw.ref, igwattachment);
     publicSubnetC.addDefaultInternetRoute(igw.ref, igwattachment);
 
+    // yum update need internet connectvity. 
+    // we should make connection between private subnet and at least one nat gateway.
+    const ngw = publicSubnetA.addNatGateway();
+    privateSubnetA.addDefaultNatRoute(ngw.ref);
+    privateSubnetC.addDefaultNatRoute(ngw.ref);
+
     const sshandhttpsg = new ec2.SecurityGroup(this, 'sshandhttpsg', {
       vpc: vpc,
       securityGroupName: 'test-alb-sg-ssh-http',
@@ -74,7 +87,41 @@ export class CdkSampleTsStack extends cdk.Stack {
       storage: ec2.AmazonLinuxStorage.GENERAL_PURPOSE
     });
 
-    const userdata = ec2.UserData.custom("sudo yum update -y \nsudo yum install -y httpd24 php56 php56-mysqld \nsudo chkconfig httpd on\nsudo groupadd www\nsudo usermod -a -G www ec2-user\nsudo chgrp -R www /var/www\nsudo chmod 2775 /var/www\n");
+    /**
+     * '''shell
+      #!/bin/bash -ex
+      exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+      echo BEGIN_USERSCRIPT
+      date '+%Y-%m-%d %H:%M:%S'
+      sudo yum update -y
+      sudo yum install -y httpd php php-mysqld 
+      sudo chkconfig httpd on
+      sudo groupadd www
+      sudo usermod -a -G www ec2-user
+      sudo chgrp -R www /var/www
+      sudo echo `ifconfig | grep inet` >> /var/www/html/index.html
+      sudo chmod 2775 /var/www
+      sudo service httpd start
+      echo END_USERSCRIPT
+     * '''shell
+     */
+    const userdata = ec2.UserData.forLinux({
+      shebang: `#!/bin/bash -ex
+      exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+      echo BEGIN_USERSCRIPT
+      date '+%Y-%m-%d %H:%M:%S'
+      sudo yum update -y
+      sudo yum install -y httpd php php-mysqld 
+      sudo chkconfig httpd on
+      sudo groupadd www
+      sudo usermod -a -G www ec2-user
+      sudo chgrp -R www /var/www
+      sudo echo \`ifconfig | grep inet\` >> /var/www/html/index.html
+      sudo chmod 2775 /var/www
+      sudo service httpd start
+      echo END_USERSCRIPT
+      `
+    });
 
     const ec2insta = new ec2.Instance(this, 'insta', {
       instanceType : ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
@@ -96,13 +143,45 @@ export class CdkSampleTsStack extends cdk.Stack {
       vpc: vpc,
       userData: userdata,
       allowAllOutbound: true,
-      instanceName: 'insta',
+      instanceName: 'instc',
       keyName: 'sample_keypair',
       securityGroup: sshandhttpsg,
       vpcSubnets: {
         subnets: [privateSubnetC]
       }
     });
+
+    // add ALB and alb target class
+    const albtarget = new elbv2.ApplicationTargetGroup(this, `${elemPrefix}-albtg1`, {
+      targetGroupName: `${elemPrefix}-albtg1`,
+      port: 80,
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      targetType: elbv2.TargetType.INSTANCE,
+      targets: [ new elbv2.InstanceTarget(ec2insta.instanceId), new elbv2.InstanceTarget(ec2instc.instanceId)],
+      healthCheck : {
+        // 5 seconds could make troubles in future.
+        timeout: cdk.Duration.seconds(10)
+      },
+      vpc: vpc
+    });
+
+    const alb = new elbv2.ApplicationLoadBalancer(this, `${elemPrefix}-alb1`, {
+      vpc: vpc, 
+      internetFacing: true, 
+      vpcSubnets: {
+        subnets: [publicSubnetA, publicSubnetC]
+      }
+    });
+
+    const listener = alb.addListener(`${elemPrefix}-httplistener`, {
+      port: 80
+    });
+
+    listener.addTargetGroups(`${elemPrefix}-attachedtg1`, {
+      targetGroups: [albtarget]
+    });
+
+    listener.connections.allowDefaultPortFromAnyIpv4('open to the world.');
 
   }
 }
